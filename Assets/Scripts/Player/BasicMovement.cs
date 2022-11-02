655786723944871391cf5c2c73d97a1f400d81d7
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2022-11-02 15:46:51 +0000

    Basic animations

---
+++ Assets/Scripts/Player/BasicMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        //horizontal movement
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+        //vertical movement
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + vertical * Time.deltaTime;
+    }
+}
