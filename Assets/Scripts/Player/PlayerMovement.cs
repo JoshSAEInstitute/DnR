f25dc7e9cf4aacf58170a6a2c4cde0aceefd07f4
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2022-11-09 12:25:39 +0000

    Dashing
    Player can now dash

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,10 +12,21 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private LayerMask jumpableGround;
 
+    //Basic Movements
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float jumpForce = 14f;
 
+    //Dashing
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    [SerializeField] private float dashingCooldown = 1f;
+
+    [SerializeField] private TrailRenderer tr;
+
+    //Animation
     private enum movementState { idle, running, jumping, fall}
     
 
@@ -31,6 +42,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
+
         dirX = Input.GetAxis("Horizontal");
         RB.velocity = new Vector2 (dirX * moveSpeed, RB.velocity.y);
 
@@ -39,9 +56,16 @@ public class PlayerMovement : MonoBehaviour
             RB.velocity = new Vector2(RB.velocity.x, jumpForce);
         }
 
-        updateAnimation();
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+
+
         //Calls the animation
-       
+        updateAnimation();
+        
     }
 
     private void updateAnimation()
@@ -85,5 +109,27 @@ public class PlayerMovement : MonoBehaviour
         */
 
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = RB.gravityScale;
+        RB.gravityScale = 0f;
+        if (!sprite.flipX)
+        {
+            RB.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        } else if (sprite.flipX)
+        {
+            RB.velocity = new Vector2(transform.localScale.x * -dashingPower, 0f);
+        }
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        RB.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
