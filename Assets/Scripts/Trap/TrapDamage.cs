6a25920578592d6c580fcab5ea07f0c2932bf119
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2022-11-30 16:51:27 +0000

    Strawberry
    Added a strawberry

--- Assets/Scripts/Trap/TrapDamage.cs
+++ Assets/Scripts/Trap/TrapDamage.cs
@@ -7,18 +7,6 @@ public class TrapDamage : MonoBehaviour
 
     [SerializeField] private int damage;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
