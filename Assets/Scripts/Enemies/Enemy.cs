8c96eda1b40c24c8f0fc30d20ded9895c08a5c4b
JoshGonzales <2009035@brunel.ac.uk>
2022-12-07 13:07:10 +0000

    Collectible and Level design

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    //Enemy speed
+    public float speed;
+
+    //Enemy line of sight
+    public float lineOfSight;
+    private Transform player;
+
+    //Faceplayer
+    private Vector3 tempScale;
+
+    //Health
+    public int maxHP;
+    public int currentHP;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        currentHP = maxHP;
+
     }
 
     // Update is called once per frame
